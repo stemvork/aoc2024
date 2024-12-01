35691d1a92c72a7b43538447b6da3c683cbb8ed2
Jasper Eenhoorn <een@emmauscollege.nl>
2024-12-01 14:11:11 +0100

    updated template for day 2

---
+++ 2.js
@@ -0,0 +1,24 @@
+const day = 2;
+const zeroPad = (num, places) => String(num).padStart(places, '0');
+
+console.log(`Day ${zeroPad(day,2)} part 1: test case =`, solve1(sample()));
+console.log(`Day ${zeroPad(day,2)} part 1: real case =`, solve1(input()));
+
+//console.log(`Day ${zeroPad(day,2)} part 2: test case =`, solve2(sample()));
+//console.log(`Day ${zeroPad(day,2)} part 2: real case =`, solve2(input()));
+
+function solve1(data) {
+    const lines = data.split('\n');
+}
+
+function solve2(data) {
+    const lines = data.split('\n');
+}
+
+function sample() {
+    return ``;
+}
+
+function input() {
+    return ``;
+}
